9650f5d5191901c2399926baa3d580f1937024f5
sscottie <trepadeira0ru@gmail.com>
2021-02-02 23:36:13 +0300

    added solvers

--- src/main/java/com/Computer_v1/Solver.java
+++ src/main/java/com/Computer_v1/Solver.java
@@ -1,25 +1,35 @@
 package com.Computer_v1;
 
 public class Solver {
-    Expression expression;
+	Expression expression;
 
-    public Solver(Expression expression) {
-        this.expression = expression;
-        if (expression.polynomialDegree == 1) {
-            solverLinear();
-        } else if (expression.polynomialDegree == 2) {
-            solverParabola();
-        } else {
-            System.out.println("It cannot solve higher degree yet");
-        }
+	public Solver(Expression expression) {
+		this.expression = expression;
+		if (expression.polynomialDegree == 1 && expression.expression[1].mult != 0) {
+			solveLinear();
+		} else if (expression.polynomialDegree == 2) {
+			solveParabola();
+		} else {
+			System.out.println("It cannot solve higher degree yet or Division by zero exception");
+		}
+	}
+
+	private void solveLinear() {
+        System.out.println("The solution is:");
+        System.out.println((expression.expression[0].mult * -1) / expression.expression[1].mult);
     }
-
-    private void solverLinear() {
-
-    }
-
-    private void solverParabola() {
-        System.out.println("Solving");
-    }
+	private void solveParabola() {
+		double d = (expression.expression[1].mult * expression.expression[1].mult) - (4 * (expression.expression[2].mult * expression.expression[0].mult));
+		if (d > 0) {
+			System.out.println("Discriminant is strictly positive, the two solutions are:");
+			System.out.println(((expression.expression[1].mult * - 1) + Math.sqrt(d)) / (expression.expression[0].mult * 2));
+			System.out.println(((expression.expression[1].mult * - 1) - Math.sqrt(d)) / (expression.expression[0].mult * 2));
+		} else if (d == 0) {
+			System.out.println("The solution is:");
+			System.out.println((expression.expression[1].mult * - 1) / (expression.expression[0].mult * 2));
+		} else {
+			System.out.println("There is no solution");
+		}
+	}
 
 }
