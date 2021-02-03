c50cf1e613473324a492d83969d526494a8b6438
Sherry Scottie <sscottie@am-a3.msk.21-school.ru>
2021-02-03 21:34:15 +0300

    tryed to add cube but fail. 2nd degree works as the 1 degree. #TODO break up tests, and add Zero fuckers

--- src/main/java/com/Computer_v1/Computer_V1.java
+++ src/main/java/com/Computer_v1/Computer_V1.java
@@ -5,19 +5,19 @@ import java.util.regex.Pattern;
 
 public class Computer_V1 {
 
-    public static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.println("./usage");
-            return;
-        }
-        if (args.length == 1) {
-            Expression expression = new Expression(args[0]);
-            String[] expression_parts = args[0].split(" = ");
-            expression.parse_left(expression_parts[0].replace(" ", ""));
-            expression.parse_right(expression_parts[1].replace(" ", ""));
-            expression.formatedPrint();
-            Solver solver = new Solver(expression);
-            return;
-        }
-    }
+	public static void main(String[] args) {
+		if (args.length == 0) {
+			System.out.println("./usage");
+			return;
+		}
+		if (args.length == 1) {
+			Expression expression = new Expression(args[0]);
+			String[] expression_parts = args[0].split(" = ");
+			expression.parse_left(expression_parts[0].replace(" ", ""));
+			expression.parse_right(expression_parts[1].replace(" ", ""));
+			expression.formatedPrint();
+			Solver solver = new Solver(expression);
+			return;
+		}
+	}
 }
--- src/main/java/com/Computer_v1/Solver.java
+++ src/main/java/com/Computer_v1/Solver.java
@@ -1,34 +1,46 @@
 package com.Computer_v1;
 
 public class Solver {
-	Expression expression;
+    Expression expression;
 
-	public Solver(Expression expression) {
-		this.expression = expression;
-		if (expression.polynomialDegree == 1 && expression.expression[1].mult != 0) {
-			solveLinear();
-		} else if (expression.polynomialDegree == 2) {
-			solveParabola();
-		} else {
-			System.out.println("It cannot solve higher degree yet or Division by zero exception");
-		}
-	}
+    public Solver(Expression expression) {
+        this.expression = expression;
+        if (expression.polynomialDegree == 1 && expression.expression[1].mult != 0) {
+            solveLinear();
+        } else if (expression.polynomialDegree == 2) {
+            solveParabola();
+        } else if (expression.polynomialDegree == 3) {
+			solveCube();
+        } else {
+            System.out.println("It cannot solve higher degree yet or Division by zero exception");
+        }
+    }
 
-	private void solveLinear() {
+    private void solveLinear() {
         System.out.println("The solution is:");
         System.out.println((expression.expression[0].mult * -1) / expression.expression[1].mult);
     }
-	private void solveParabola() {
-		double d = (expression.expression[1].mult * expression.expression[1].mult) - (4 * (expression.expression[2].mult * expression.expression[0].mult));
-		if (d > 0) {
-			System.out.println("Discriminant is strictly positive, the two solutions are:");
-			System.out.println(((expression.expression[1].mult * - 1) + Math.sqrt(d)) / (expression.expression[0].mult * 2));
-			System.out.println(((expression.expression[1].mult * - 1) - Math.sqrt(d)) / (expression.expression[0].mult * 2));
-		} else if (d == 0) {
-			System.out.println("The solution is:");
-			System.out.println((expression.expression[1].mult * - 1) / (expression.expression[0].mult * 2));
-		} else {
-			System.out.println("There is no solution");
+
+    private void solveParabola() {
+        double d = (expression.expression[1].mult * expression.expression[1].mult) - (4 * (expression.expression[2].mult * expression.expression[0].mult));
+        if (d > 0) {
+            System.out.println("Discriminant is strictly positive, the two solutions are:");
+            System.out.println(((expression.expression[1].mult * -1) + Math.sqrt(d)) / (expression.expression[0].mult * 2));
+            System.out.println(((expression.expression[1].mult * -1) - Math.sqrt(d)) / (expression.expression[0].mult * 2));
+        } else if (d == 0) {
+            System.out.println("The solution is:");
+            System.out.println((expression.expression[1].mult * -1) / (expression.expression[0].mult * 2));
+        } else {
+            System.out.println("There is no solution");
+        }
+    }
+
+    private void solveCube() {
+		for (int x = -100; x <= 100; x++) {
+			double result = expression.expression[3].mult * Math.pow(x, 3) + expression.expression[2].mult * x * x + expression.expression[1].mult * x + expression.expression[0].mult;
+			if (result == 0){
+				System.out.println(x);
+			}
 		}
 	}
 
